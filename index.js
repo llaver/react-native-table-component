06a325a967e3969e5c7e4e17ecdcb9fe3fda5bfd
Ryan Bell <llaver@live.com>
2021-02-11 00:23:35 -0600

    Fix exports

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export { Row, Rows } from './components/rows';
-export { Col, Cols } from './components/cols';
-export { Table, TableWrapper } from './components/table';
-export { Cell } from './components/cell';
+export { Row, Rows } from './src/components/rows';
+export { Col, Cols } from './src/components/cols';
+export { Table, TableWrapper } from './src/components/table';
+export { Cell } from './src/components/cell';
